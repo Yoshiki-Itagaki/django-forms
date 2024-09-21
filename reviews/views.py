741af4423f32cb4a93947798dc66e052373c8fdc
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-21 22:05:42 +0900

    Implemented Class Based Views

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views import View
 
 from reviews.models import Review
 
@@ -7,21 +8,40 @@ from .forms import ReviewForm
 
 # Create your views here.
 
-def review(request):
-    
-    if request.method == "POST":
+class ReviewView(View):
+    def get(self, request):        
+        form = ReviewForm()
+            
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+        
+    def post(self, request):
         form = ReviewForm(request.POST)
         
         if form.is_valid():         
             form.save()           
             return HttpResponseRedirect("/thank-you")
-        
-    else:
-        form = ReviewForm()
+
+        return render(request, "reviews/review.html", {
+            "form": form
+        })
+
+# def review(request):
     
-    return render(request, "reviews/review.html", {
-        "form": form
-    })
+#     if request.method == "POST":
+#         form = ReviewForm(request.POST)
+        
+#         if form.is_valid():         
+#             form.save()           
+#             return HttpResponseRedirect("/thank-you")
+        
+#     else:
+#         form = ReviewForm()
+    
+#     return render(request, "reviews/review.html", {
+#         "form": form
+#     })
 
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
