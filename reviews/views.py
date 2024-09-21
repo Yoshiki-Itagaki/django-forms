2603fcec81911efa98c49b147755d0ad81246eec
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 02:36:39 +0900

    Implemented a FormView

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 
 from reviews.models import Review
 
@@ -11,25 +12,15 @@ from .forms import ReviewForm
 
 # Create your views here.
 
-class ReviewView(View):
-    def get(self, request):        
-        form = ReviewForm()
-            
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-        
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        
-        if form.is_valid():         
-            form.save()           
-            return HttpResponseRedirect("/thank-you")
-
-        return render(request, "reviews/review.html", {
-            "form": form
-        })
-        
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = "reviews/review.html"
+    success_url = "/thank-you"
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)    
+    
 class ThankYouView(TemplateView):
     template_name="reviews/thank_you.html"
     
@@ -48,15 +39,11 @@ class ReviewsListView(ListView):
     #    data = base_query.filter(rating__gte=4)
     #    return data          
     
-class SingleReviewView(TemplateView):     
+class SingleReviewView(DetailView):     
     template_name = "reviews/single_review.html"
+    model = Review
     
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        review_id = kwargs["id"]
-        selected_review = Review.objects.get(pk=review_id)
-        context["review"] = selected_review
-        return context
+   
 
 # def review(request):
     
