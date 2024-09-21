23c8b7e08731ac6cb50ddd5fdcd601e761c51531
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 00:04:39 +0900

    Applied ListView class

--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
 
 from reviews.models import Review
 
@@ -37,14 +38,15 @@ class ThankYouView(TemplateView):
         context["message"] = "This works!"
         return context
     
-class ReviewsListView(TemplateView):
+class ReviewsListView(ListView):
     template_name = "reviews/review_list.html"
+    model = Review
+    context_object_name = "reviews"
     
-    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-        context = super().get_context_data(**kwargs)
-        reviews = Review.objects.all()
-        context["reviews"] = reviews
-        return context
+    # def get_queryset(self):
+    #    base_query = super().get_queryset()
+    #    data = base_query.filter(rating__gte=4)
+    #    return data          
     
 class SingleReviewView(TemplateView):     
     template_name = "reviews/single_review.html"
