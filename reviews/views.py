95de3c82cfd1cf58422af047988d7cca5e91b7db
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-22 11:31:56 +0900

    Implemented a CreateView

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,8 @@
 from typing import Any
-from django.http import HttpResponseRedirect
-from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from reviews.models import Review
 
@@ -12,14 +10,11 @@ from .forms import ReviewForm
 
 # Create your views here.
 
-class ReviewView(FormView):
+class ReviewView(CreateView):
+    model = Review
     form_class = ReviewForm
     template_name = "reviews/review.html"
-    success_url = "/thank-you"
-    
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)    
+    success_url = "/thank-you"  
     
 class ThankYouView(TemplateView):
     template_name="reviews/thank_you.html"
@@ -43,8 +38,6 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
     
-   
-
 # def review(request):
     
 #     if request.method == "POST":
